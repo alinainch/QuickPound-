ab714f9a729d115a92c3bfd17dc6feaa5f129787
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-05-13 00:25:39 -0400

    quickpound push

---
+++ routes/posts.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../middleware/multer");
+const postsController = require("../controllers/posts");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Post Routes - simplified for now
+router.get("/:id", ensureAuth, postsController.getPost);
+
+router.post("/createPost", upload.single("file"), postsController.createPost);
+
+router.post("/createComment/:postID", postsController.createComment);
+
+router.put("/likePost/:id", postsController.likePost);
+
+router.delete("/deletePost/:id", postsController.deletePost);
+
+router.delete("/deleteComment/:postID/:commentID", postsController.deleteComment);
+
+module.exports = router;
